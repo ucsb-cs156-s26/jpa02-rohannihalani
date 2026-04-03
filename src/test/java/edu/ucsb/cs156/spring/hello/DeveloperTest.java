62ac5877cf54552db599f1bb470574195c30c962
RohanNihalani0 <rohan.ved.nihalani@gmail.com>
2026-04-02 17:50:36 -0700

    updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,8 +24,10 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         assertEquals("Rohan", Developer.getName());
     }
-
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("rohannihalani", Developer.getGithubId());
+    }
 
 }
